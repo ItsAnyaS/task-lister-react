c5b291538527b838344acdf5ff9839bbe427c49b
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-07-22 17:10:58 -0400

    added structure

---
+++ src/components/TaskForm.js
@@ -0,0 +1,13 @@
+const TaskForm = () => {
+return (
+    <div>
+        <form action="">
+            <label htmlFor="">Task Description:</label>
+            <input type="text" placeholder="Description" />
+            <input type='submit' value="Create New Task"/>
+        </form>
+    </div>
+)
+}
+
+export default TaskForm
---
+++ src/components/TaskList.js
@@ -0,0 +1,13 @@
+import Task from "./Task";
+
+const TaskList = () => {
+return (
+  <div>
+    <h3>My To-do's</h3>
+    <ul>
+    </ul>
+  </div>
+);
+}
+
+export default TaskList
